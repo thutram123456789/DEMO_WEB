105a1348758d96d603641ce0e1fa51b4eb6566fb
thu tram <thutram@.comm>
2022-07-17 18:02:52 +0700

    cai dat express thong bao xin chao

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const cors = require ("cors");
+
+const app = express();
+
+app.use (cors());
+app.use (express.json());
+
+app.get ("/", (req,res) => {
+    res.json ({message: "Xin chao den voi ung dung quan ly danh ba!"});
+});
+
+module.exports=app;
